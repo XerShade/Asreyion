e5b0f380c0045118abcb3ef542c6032bb92ba937
XerShade <xershade.ca@gmail.com>
2025-05-24 17:58:11 -0400

    fix: various script warnings

--- scripts/nodes/player/player.gd
+++ scripts/nodes/player/player.gd
@@ -5,7 +5,7 @@ class_name Player extends CharacterBody2D
 
 @export var movement_speed: float = 160
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	self.velocity = self._get_input_velocity().normalized() * self._get_movement_speed()
 	
 	if self.velocity != Vector2.ZERO:
@@ -13,20 +13,20 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func _get_input_velocity() -> Vector2:
-	var velocity: Vector2 = Vector2(
+	var calculated_velocity: Vector2 = Vector2(
 		Input.get_axis("MOVEMENT_LEFT", "MOVEMENT_RIGHT"),
 		Input.get_axis("MOVEMENT_UP", "MOVEMENT_DOWN")
 	)
 	
-	return velocity
+	return calculated_velocity
 
 func _get_movement_speed() -> float:
-	var movement_speed: float = self.movement_speed
+	var calculated_movement_speed: float = self.movement_speed
 	
 	if(Input.is_action_pressed("MODIFIER_SPRINT")):
-		movement_speed *= 2
+		calculated_movement_speed *= 2
 	
 	if(Input.is_action_pressed("MODIFIER_SNEAK")):
-		movement_speed /= 2
+		calculated_movement_speed /= 2
 		
-	return movement_speed
+	return calculated_movement_speed
