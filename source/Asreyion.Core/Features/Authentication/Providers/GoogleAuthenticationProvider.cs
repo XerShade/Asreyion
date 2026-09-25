c0c0d44017186d15288751b7416a0bcad3dab539
XerShade <xershade.ca@gmail.com>
2026-09-25 11:57:50 -0400

    Update GoogleAuthenticationProvider.cs

--- source/Asreyion.Core/Features/Authentication/Providers/GoogleAuthenticationProvider.cs
+++ source/Asreyion.Core/Features/Authentication/Providers/GoogleAuthenticationProvider.cs
@@ -1,4 +1,10 @@
-﻿using Asreyion.Core.Features.Authentication.Providers.Interfaces;
+﻿/* Disabled Google Authentication Support
+ * Reason: It shall remain disabled as of until such time that Lake Ontario is not shown as "Lake America" in ANY country.
+ * Note: Yes I am leaving the code here in the project. It is just disabled and not used when the project is built.
+ *       I'll re-enable it when Lake Ontario is not shown as "Lake America" in any country. If you use my code and want to
+ *       bend the knee feel free, just remove the comments and enable the code and it will work again as if never disabled.
+ *       
+using Asreyion.Core.Features.Authentication.Providers.Interfaces;
 using Microsoft.AspNetCore.Authentication;
 
 namespace Asreyion.Core.Features.Authentication.Providers;
@@ -26,3 +32,4 @@ public sealed class GoogleAuthenticationProvider : IAuthenticationProvider
         });
     }
 }
+*/
